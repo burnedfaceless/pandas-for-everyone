4fad0dde823ff71b41c63a00abcd334143d197c8
Brian Abbott <hi@brianabbott.com>
2023-03-05 16:23:51 -0500

    added 3 and 4

---
+++ chapter-1/4-df-columns.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# by default, read_csv() assumes that the separator is a comma
+# if you want to use a tab, you need to specify it
+df = pd.read_csv('gapminder.tsv', sep='\t')
+
+# get column names
+print(df.columns)
